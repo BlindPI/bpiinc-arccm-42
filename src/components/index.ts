16ab9e7c5ca3047dda4cb261eee302609f90a4da
BlindPI <blindpigeonindustries@gmail.com>
2025-05-19 13:11:35 -0400

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+// File: src/components/index.ts
+export { default as LocationForm } from './LocationForm';
+export * from './LocationForm';
+
+export { default as LocationTable } from './LocationTable';
+export * from './LocationTable';
+
+// Export other components as needed
