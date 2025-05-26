1f9fcc5f495b9b002581443c82b033c0362d0df8
BlindPI <blindpigeonindustries@gmail.com>
2025-05-26 14:40:49 -0400

    Delete bpiinc-arccm-42-main/bpiinc-arccm-42-main directory

--- bpiinc-arccm-42-main/bpiinc-arccm-42-main/src/components/CertificateForm.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import { CertificateForm as CertificateFormComponent } from './certificates/CertificateForm';
-
-export function CertificateForm() {
-  return <CertificateFormComponent />;
-}
